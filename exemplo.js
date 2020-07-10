b5afebfd525dcf593f68bb20cec28be37682f878
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-07-10 20:15:02 -0300

    first commit

---
+++ exemplo.js
@@ -0,0 +1,32 @@
+// Extrutura de uma variável:
+//      var nome_da_variavel = VALOR;
+//      var nome_da_variavel;
+
+// Palavra(s) equivalente String
+var string = "Palavra qualquer";
+// Numero, equivalente Numeric (Int, Float, Double)
+var numero = 3;
+var numero_2 = 3.15;
+var numero_3 = -3.15;
+// Boleano é um verdadeiro ou falso, em inglês;
+var booleano = true;
+var booleano_2 = false;
+
+// Funções,
+var funcao = function() { /** Conteudo da funcao */}
+// indefinido,
+var indefinida;
+// null, 
+var nula = null;
+// Matriz ou Objetos
+var matriz = ["segunda", "terca", "sexta", 5, true];
+var objeto = {nome: "Thiago", idade: 37};
+
+// Trazer valores dentro de um array
+console.log("VALOR DA VARIAVEL matriz true:");
+console.log(matriz[4]);
+
+// Trazer valor de um objeto literal
+console.log("Valor do nome no objeto literal:");
+console.log(objeto["nome"]);
+// console.log(objeto.nome);
